3c810933ea47db4a070dc3f2f918625c218eccd4
Johannes Berggren <username@users.noreply.github.com>
2018-01-08 13:05:10 +0100

    Partially rewritten classifier - work in progress TODO: Populate a DenseInstance with 120 values received over Bluetooth connection and use it as input for classification. BUG: Exception running weka library
    Completed code for reception of bluetooth data.
    TODO: Change handler code from writing to log, should instead add values to a DenseInstance object that is used for classification when full.
    TODO: Clean up bluetooth connection code.
    
    TODO: When a gesture is classified, send it on to the NodeMCU in command format (see constants).

--- app/src/main/java/se/mah/m11p0121/iotproject/Classifier.java
+++ app/src/main/java/se/mah/m11p0121/iotproject/Classifier.java
@@ -1,40 +1,73 @@
 package se.mah.m11p0121.iotproject;
 
+import android.content.Context;
+import android.util.Log;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+//import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
+//import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Random;
 
 import weka.classifiers.trees.J48;
+import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instance;
-import weka.core.*;
+import weka.core.Instances;
 
-public class Classifier {
+class Classifier {
     private J48 tree = new J48();         // new instance of tree
     private int classIndex;
+    // create a DenseInstance based on your live Acc and Gyr data
+    private DenseInstance instance = new DenseInstance(121); // assuming that you have 120 values + one class label
+    private int newValueIndex = 0;
 
-    public void setupClassifier() throws Exception {
+    Classifier(Context context) {
         // load training data
-        BufferedReader breader = null;
+        BufferedReader breader;
+        InputStream arffStream = context.getResources().openRawResource(R.raw.train_small);
         // put the address of your training file here
-        breader = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/bin/weka/api/train.arff"));
-        Instances train = new Instances(breader);
-        classIndex = train.numAttributes() - 1;
-        train.setClassIndex(classIndex);
+        try {
 
-        // build classifier
-        tree.buildClassifier(train);
+            breader = new BufferedReader(new InputStreamReader(arffStream));
+            Instances train = new Instances(breader);
+            classIndex = train.numAttributes() - 1;
+            train.setClassIndex(classIndex);
+
+            // build classifier
+            tree.buildClassifier(train);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                arffStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
-    public Instances newInstances() {
-        // create a DenseInstance based on your live Acc and Gyr data
-        DenseInstance instance = new DenseInstance(121); // assuming that you have 120 values + one class label
+    void addValues(String[] newValues) {
+        for(int i = 1; i < newValues.length; i++) {
+            instance.setValue(newValueIndex++, Integer.parseInt(newValues[i]));
+            if(newValueIndex== 120) {
+                try {
+                    classify(newInstances(instance));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                instance = new DenseInstance(121);
+                newValueIndex = 0;
+            }
+        }
+    }
 
-
-
+    private Instances newInstances(DenseInstance instance) {
         // create instances for the live instance: first you need to create the attributes
         ArrayList<Attribute> attributes = new ArrayList<>();
         for (int i = 1; i < 21; i++) {
@@ -64,10 +97,10 @@ public class Classifier {
         return unlabeled;
     }
 
-    public void classify(Instances unclassified) throws Exception {
+    private void classify(Instances unclassified) throws Exception {
 
         double clsLabel = tree.classifyInstance(unclassified.instance(0));
         unclassified.instance(0).setClassValue(clsLabel);
-        System.out.println("Detected Gesture: " + unclassified.instance(0).attribute(classIndex).value((int) clsLabel));
+        Log.d("GESTURECLASS" , "Detected Gesture: " + unclassified.instance(0).attribute(classIndex).value((int) clsLabel));
     }
 }
